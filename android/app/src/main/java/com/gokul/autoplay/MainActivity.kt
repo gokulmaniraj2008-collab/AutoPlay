79c6d4c960dc06de07f3985a3c44dc3a3eaaa7d3
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-14 15:11:24 +0530

    feat: add Android activity foundation

---
+++ android/app/src/main/java/com/gokul/autoplay/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.gokul.autoplay
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
