31d86f68410f0f5c98b1274ae3e5eaae31ba90d7
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-14 17:07:24 +0530

    feat(android): add Supabase cloud config

---
+++ android/app/src/main/java/com/gokul/autoplay/SupabaseConfig.kt
@@ -0,0 +1,6 @@
+package com.gokul.autoplay
+
+object SupabaseConfig {
+    const val URL = "https://bqrpgtxtmatxwtdpuoyh.supabase.co"
+    const val PUBLISHABLE_KEY = "sb_publishable_Dl_Fltbnshhahd9rbkMgxw_NPtn4pM5"
+}
