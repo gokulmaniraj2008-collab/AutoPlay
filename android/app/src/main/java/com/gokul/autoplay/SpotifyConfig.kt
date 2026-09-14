0c90089621197d38c76fe3dbf8ec13fb1760b20d
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-14 17:18:32 +0530

    refactor: remove Spotify configuration

--- android/app/src/main/java/com/gokul/autoplay/SpotifyConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.gokul.autoplay
-
-object SpotifyConfig {
-    const val REDIRECT_URI = "autoplay://spotify/callback"
-    val clientId: String
-        get() = BuildConfig.SPOTIFY_CLIENT_ID
-}
