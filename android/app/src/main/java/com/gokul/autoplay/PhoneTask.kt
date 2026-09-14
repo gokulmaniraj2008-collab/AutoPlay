a67fae4eaf825303c0c1cdf162de4f72c5d25eb9
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-14 18:55:02 +0530

    feat(android): add normalized phone task model

---
+++ android/app/src/main/java/com/gokul/autoplay/PhoneTask.kt
@@ -0,0 +1,19 @@
+package com.gokul.autoplay
+
+/** A normalized user task that AutoPlay can execute through Android capabilities. */
+data class PhoneTask(
+    val id: String,
+    val rawCommand: String,
+    val action: Action,
+    val target: String = "",
+    val scheduledAtMillis: Long? = null,
+    val enabled: Boolean = true
+) {
+    enum class Action {
+        PLAY_SPOTIFY,
+        CALL,
+        OPEN_APP,
+        OPEN_URL,
+        UNKNOWN
+    }
+}
