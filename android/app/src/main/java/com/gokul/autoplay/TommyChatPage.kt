42258eab3a2c0d49d70eda7cbda1f7e25f388e7a
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-15 08:39:54 +0530

    Keep Spotify search results inside Tommy Chat

--- android/app/src/main/java/com/gokul/autoplay/TommyChatPage.kt
+++ android/app/src/main/java/com/gokul/autoplay/TommyChatPage.kt
@@ -32,7 +32,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -283,9 +282,9 @@ private suspend fun runTommyCommand(
                         spotifyResults = results
                     )
                 )
-            } else {
-                messages.add(TommyMessage(true, "I couldn't find Spotify results for $query."))
+                return
             }
+            messages.add(TommyMessage(true, "I couldn't load live Spotify results. I'll open Spotify search instead."))
         }
     }
 
